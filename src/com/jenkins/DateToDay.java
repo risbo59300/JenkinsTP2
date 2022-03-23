d003d4ff73e62f3f4a9fdd13a1f090d609b33529
GABA-MANO <ybgabamano@gmail.com>
2022-03-23 17:22:39 +0100

    first commit

---
+++ src/com/jenkins/DateToDay.java
@@ -0,0 +1,23 @@
+package com.jenkins;
+
+import java.util.Date;
+
+public class DateToDay {
+
+    Date now = new Date();
+
+    int mm = now.getMonth();
+
+    int dd = now.getDay();
+
+    int yyyy = now.getYear();
+
+    int hour = now.getHours();
+
+    int mi = now.getMinutes();
+
+    int ss = now.getSeconds();
+
+
+
+}
